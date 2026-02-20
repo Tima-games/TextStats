9a4bafe040c555f8efe93cbc137f89e9e902ae52
Tima_games <petrulantima@gmail.com>
2026-02-21 01:15:53 +0300

    version 1.2: file input + EOF handling

--- main.go
+++ main.go
@@ -3,28 +3,43 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"strings"
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
+
+	var reader *bufio.Reader
+
+	if len(os.Args) > 1 {
+		file, err := os.Open(os.Args[1])
+		if err != nil {
+			fmt.Println("Error opening file:", err)
+			return
+		}
+		defer file.Close()
+		reader = bufio.NewReader(file)
+	} else {
+		reader = bufio.NewReader(os.Stdin)
+	}
 
 	length := 0
 	space := 0
 	lines := 0
-	wordc := 0
+	wordCount := 0
 
 	for {
 		line, err := reader.ReadString('\n')
-		if err != nil {
+		if err != nil && err != io.EOF {
+			fmt.Println("Error reading line:", err)
 			break
 		}
 		lines++
 		line = strings.TrimSpace(line)
 
 		words := strings.Fields(line)
-		wordc += len(words)
+		wordCount += len(words)
 
 		for _, r := range line {
 			if r == ' ' {
@@ -33,9 +48,12 @@ func main() {
 				length++
 			}
 		}
+		if err == io.EOF {
+			break
+		}
 	}
 	fmt.Println("In your string(s)", lines, "lines")
-	fmt.Println("In your string(s)", wordc, "words")
+	fmt.Println("In your string(s)", wordCount, "words")
 	fmt.Println("In your string(s)", space, "spaces")
 	fmt.Println("In your string(s)", length, "letters")
 }
